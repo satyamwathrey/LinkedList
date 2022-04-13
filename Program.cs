040ca3bb0c2040084e810b2d3abeea385f0111b5
satyamwathrey <swathrey1@gmail.com>
2022-04-13 18:12:18 +0530

    Ability to sort Linked List in ascending order of sequence 56->30->40->70

--- Program.cs
+++ Program.cs
@@ -11,48 +11,54 @@ namespace LinkedList
             Console.WriteLine("=-=-=-=-=-Welcome To The Linked List Program-=-=-=-=-=");
             Console.WriteLine("-----------------------------------------------");
 
-            //Calling Class LinkedList
+            //Object of Class Sorte LinkedList
+            SortedLinkedList sortedLinkedList = new SortedLinkedList();
+            sortedLinkedList.Add(56);
+            sortedLinkedList.Add(30);
+            sortedLinkedList.Add(40);
+            sortedLinkedList.Add(70);
+
+            Console.WriteLine("Ordered Linked List in Ascending Order ::");
+            Console.WriteLine("-----------------------------------------");
+
+            sortedLinkedList.Display();
+            /*//Calling Class LinkedList
             LinkedList linkedList = new LinkedList();
             linkedList.Add(56);
             linkedList.Add(30);
             linkedList.Add(70);
-
             //Searching an Element            
-            linkedList.SearchElement(30);
-
+            linkedList.SearchElement(30);            
+            
             //Adding Data After 30
             linkedList.InsertAtParticularPosition(3, 40);
-
-            /* //Display Data Before Delete Last element 
+            
+            *//* //Display Data Before Delete Last element 
              Console.WriteLine("-----------------------------------------------");
              Console.WriteLine("Display Data Before Delete Last element ::");
              Console.WriteLine("-----------------------------------------------");
              linkedList.Display();
              //Delete Last Element
              linkedList.PopLastElement();
-            */
-
-            //Display Data After Add
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("Display After Adding element ::");
-            Console.WriteLine("-----------------------------------------------");
-
-            Console.WriteLine("Data In Linked List");
-            linkedList.Display();
-
+            *//*
+             //Display Data After Add
+             Console.WriteLine("-----------------------------------------------");
+             Console.WriteLine("Display After Adding element ::");
+             Console.WriteLine("-----------------------------------------------");
+            
+             Console.WriteLine("Data In Linked List");
+             linkedList.Display();
             //Delete element and then get size
             linkedList.Delete(40);
-
+            
             //Display Data After Add
-            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("-----------------------------------------------");            
             Console.WriteLine("Display After Delete element ::");
             Console.WriteLine("Size of Linked List : " + linkedList.Size());
             Console.WriteLine("-----------------------------------------------");
-
             Console.WriteLine("Data In Linked List");
             linkedList.Display();
-
-
+*/
             Console.ReadLine();
 
         }
